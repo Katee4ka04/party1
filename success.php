725798a553f225bc18a46861cbbb02673dd54673
Katee4ka04 <31989266+Katee4ka04@users.noreply.github.com>
2018-05-18 14:53:02 +0300

    thjo[3h];jethjo

--- success.php
+++ success.php
@@ -3,4 +3,4 @@ require 'vendor/autoload.php';
 $app = new \atk4\ui\App('SnowwA!');
 $app->initLayout("Centered");
 
-$msg=$app->add(['Message','Заходи, добрый человек гостем будешь!', 'type'=>'succes','icon'=>'loading loud download']);
+$msg=$app->add(['Message','Заходи, добрый человек гостем будешь!', 'type'=>'succes','icon'=>'loading loud download','teal']);
