d5942ffff09e66236df8d7033c267cf3c8bcb860
Katee4ka04 <31989266+Katee4ka04@users.noreply.github.com>
2018-05-18 14:37:52 +0300

    fgfgfgf

--- connection.php
+++ connection.php
@@ -4,7 +4,7 @@ session_start();
 $db = new \atk4\data\Persistence_SQL('mysql:dbname=heroku_35a63eb77fee245;host=eu-mm-auto-fra-02-c.cleardb.net','bfa11752c77b7d','77c43c35');
 //$db = new \atk4\data\Persistence_SQL
 class Friends extends \atk4\data\Model {
-  public $table = 'Anna';
+  public $table = 'anna';
   function init () {
     parent::init();
     $this->addField('name');
